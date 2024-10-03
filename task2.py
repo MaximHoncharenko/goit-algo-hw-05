82a2edf35174157f1d5bba49898062e12f2d817a
Max Honcharenko <157576499+MaximHoncharenko@users.noreply.github.com>
2024-10-03 17:50:29 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,7 +2,7 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    pattern = r'\d+\.\d+'
+    pattern = r' \d+\.\d+ '
     for match in re.finditer(pattern, text):
         yield float(match.group())
 
